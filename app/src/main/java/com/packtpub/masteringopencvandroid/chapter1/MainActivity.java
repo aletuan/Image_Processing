74b2c62eebaa4112ba803a466f7f8a89714ed913
Le Tuan Anh <anhlt1983@gmail.com>
2016-06-13 11:26:55 +0700

    correct intent to select image

--- app/src/main/java/com/packtpub/masteringopencvandroid/chapter1/MainActivity.java
+++ app/src/main/java/com/packtpub/masteringopencvandroid/chapter1/MainActivity.java
@@ -2,15 +2,27 @@ package com.packtpub.masteringopencvandroid.chapter1;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.core.Mat;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class MainActivity extends Activity {
+
+    public static final String TAG = "Chapter1::MainActivity";
 
     private final int SELECT_PHOTO = 1;
     private static int ACTION_MODE = 0;
@@ -52,5 +64,46 @@ public class MainActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_load_image) {
+            //Intent photoPickerIntent = new Intent(Intent.ACTION_GET_CONTENT);
+            Intent photoPickerIntent = new Intent();
+
+            photoPickerIntent.setType("image/*");
+            photoPickerIntent.setAction(Intent.ACTION_GET_CONTENT);
+
+            startActivityForResult(Intent.createChooser(photoPickerIntent,"Select Any"), SELECT_PHOTO);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent imageReturnedIntent) {
+        super.onActivityResult(requestCode, resultCode, imageReturnedIntent);
+
+        switch(requestCode) {
+            case SELECT_PHOTO:
+                if (resultCode == RESULT_OK && imageReturnedIntent != null && imageReturnedIntent.getData() != null) {
+                    Uri imgUri = imageReturnedIntent.getData();
+
+                    try {
+                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imgUri);
+                        Log.d(TAG, String.valueOf(bitmap));
+                        //ImageView imageView = (ImageView) findViewById(R.id.imageView);
+                        //imageView.setImageBitmap(bitmap);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+                break;
+        }
     }
 }
